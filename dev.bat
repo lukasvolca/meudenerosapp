63516f72b025431ae8246bd31e90a42933f81e0d
lukasvolca <lukasvolca@gmail.com>
2026-05-06 18:54:42 -0300

    chore: adiciona .gitignore, exclui dev.bat do rastreamento

--- dev.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Iniciando servidor local...
-echo Acesse: http://localhost:3000/meudenerosapp/
-echo Pressione Ctrl+C para parar.
-cd /d "%~dp0.."
-npx serve . --listen 3000 --no-clipboard
